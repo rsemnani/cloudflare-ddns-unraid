b0a0a64499a18f2e12d355106d5b81368e645359
rsemnani <rsemnani@gmail.com>
2023-08-27 22:39:47 -0600

    Update a single zone's/domain's IPv4 record on Cloudflare

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# cloudflare-ddns-unraid
+# Cloudflare DDNS Update for Unraid
+
+A script to help Unraid users automatically update their dynamic IP on Cloudflare.
+
+## Prerequisites:
+
+1. Install Python on Unraid through the nerdpack plugin: 
+   - python3-3.9.6-x86_64-1.txz 
+   - python-pip-21.2.3-x86_64-1.txz 
+   
+2. After installation, run:
+   ```bash
+   pip3 install requests
+
+Usage:
+
+    Replace placeholders (Your Cloudflare Global API key, Your Cloudflare Login Email, Your Zone ID, and Your domain name) in ddns_update.py with your respective information.
+
+    Run the script:
+
+    bash
+
+python3 ddns_update.py
+
+Optional arguments:
+
+    --force-update: Force an update of the DNS record.
+    --force-delete-create: Force a deletion and then creation of the DNS record.
+
+Set a cron job to run the script every 6 hours:
+
+bash
+
+0 */6 * * * /path/to/python3 /path/to/ddns_update.py
---
+++ update_ipv4.py
@@ -0,0 +1,148 @@
+import requests
+import argparse
+import logging
+
+# Configure the logging module
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s')
+
+def get_args():
+    """
+    Parse command-line arguments.
+    
+    Returns:
+        argparse.Namespace: Parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="Manage Cloudflare DNS records.")
+    parser.add_argument("--force-update", action="store_true", 
+                        help="Force an update of the DNS record.")
+    parser.add_argument("--force-delete-create", action="store_true",
+                        help="Force a deletion and then creation of the DNS record.")
+    return parser.parse_args()
+
+def fetch_existing_record(zone_id, record_name, headers):
+    """
+    Fetch the existing DNS record from Cloudflare.
+
+    Args:
+        zone_id (str): Cloudflare Zone ID.
+        record_name (str): Name of the DNS record.
+        headers (dict): Required headers for Cloudflare API authentication.
+
+    Returns:
+        dict: The existing DNS record details or None if not found.
+    """
+    endpoint = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records?type=A&name={record_name}"
+    response = requests.get(endpoint, headers=headers)
+    records = response.json().get('result')
+    
+    if records:
+        return records[0]
+    return None
+
+def create_dns_record(zone_id, record_type, record_name, record_content, headers):
+    """
+    Create a new DNS record on Cloudflare.
+    """
+    data = {
+        "type": record_type,
+        "name": record_name,
+        "content": record_content,
+        "ttl": 120,  # TTL set to 2 minutes.
+        "proxied": False
+    }
+    
+    response = requests.post(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records", 
+                             headers=headers, json=data)
+    return response.json()
+
+def get_ipv4_address():
+    """
+    Fetch the public IPv4 address of the machine running the script using the ipify service.
+
+    Returns:
+        str: The public IPv4 address.
+    
+    Raises:
+        ValueError: If there's an issue fetching the IP address or parsing the response.
+    """
+    try:
+        response = requests.get("http://api.ipify.org/?format=json", headers={"Accept": "application/json"})
+        
+        # Ensure the response status code is 200 (OK)
+        response.raise_for_status()
+
+        # Parse the JSON response and return the IP address
+        return response.json()['ip']
+
+    except (requests.RequestException, KeyError) as e:
+        raise ValueError(f"Error fetching the public IPv4 address: {e}")
+
+def update_dns_record(zone_id, record_id, record_type, record_name, record_content, headers):
+    """
+    Update an existing DNS record on Cloudflare.
+    """
+    data = {
+        "type": record_type,
+        "name": record_name,
+        "content": record_content,
+        "ttl": 120,
+        "proxied": False
+    }
+
+    response = requests.put(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}",
+                            headers=headers, json=data)
+    return response.json()
+
+def delete_dns_record(zone_id, record_id, headers):
+    """
+    Delete an existing DNS record on Cloudflare.
+    """
+    response = requests.delete(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{record_id}",
+                               headers=headers)
+    return response.json()
+
+def main():
+    # Authentication and configuration details
+
+    API_KEY = 'Your Cloudflare Global API key'
+    EMAIL = 'Your Cloudflare Login Email'
+    ZONE_ID = 'Your Zone ID'
+    headers = {
+        "X-Auth-Email": EMAIL,
+        "X-Auth-Key": API_KEY,
+        "Content-Type": "application/json"
+    }
+
+    args = get_args()
+    record_type = "A"  # Insert the type of DNS record (e.g., "A" for IPv4 address, "AAAA" for IPv6, "CNAME" for canonical name, etc.)
+    record_name = "Your domain name"
+    record_content = get_ipv4_address()
+    existing_record = fetch_existing_record(ZONE_ID, record_name, headers)
+
+    if args.force_update:
+        if existing_record:
+            logging.info(f"Updating DNS record with new IP {record_content}.")
+            update_dns_record(ZONE_ID, existing_record['id'], record_type, record_name, record_content, headers)
+        else:
+            logging.info(f"Creating new DNS record with IP {record_content}.")
+            create_dns_record(ZONE_ID, record_type, record_name, record_content, headers)
+
+    elif args.force_delete_create:
+        if existing_record:
+            logging.info(f"Deleting existing DNS record and creating a new one with IP {record_content}.")
+            delete_dns_record(ZONE_ID, existing_record['id'], headers)
+        create_dns_record(ZONE_ID, record_type, record_name, record_content, headers)
+
+    else:
+        if existing_record:
+            if existing_record['content'] != record_content:
+                logging.info(f"IP has changed. Updating DNS record from {existing_record['content']} to {record_content}.")
+                update_dns_record(ZONE_ID, existing_record['id'], record_type, record_name, record_content, headers)
+            else:
+                logging.info("IP address has not changed. No update necessary.")
+        else:
+            logging.info(f"No existing DNS record found. Creating new record with IP {record_content}.")
+            create_dns_record(ZONE_ID, record_type, record_name, record_content, headers)
+
+if __name__ == "__main__":
+    main()
